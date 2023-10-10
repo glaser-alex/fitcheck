93db3119c136236b7965fe367405495714d211e4
Alex Glaser <119404263+glaser-alex@users.noreply.github.com>
2023-10-10 22:37:45 +0200

    add index

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+require "./inc/db_init.php";
+//require "./inc/checkLogin.php";
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hier entsteht FitCheck</title>
+    <style>
+        @font-face {
+            font-family: Ubuntu;
+            src: url(./font/Ubuntu/Ubuntu-Light.ttf);
+        }
+        html {
+            width: 100%;
+            height: 100%;
+            background-color: #88001b;
+            color: #fff;
+            font-family: Ubuntu, GeoSlb712MdBT, Georgia, serif;
+            font-style: normal;
+        }
+
+        .the-box {
+			text-align: center;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -60%);
+            -webkit-transform: translate(-50%, -60%);
+            -ms-transform: translate(-50%, -60%);
+        }
+
+        .the-title {
+            padding: 5px;
+            font-size: 3em;
+            text-align: center; 
+        }
+		img {
+			border-radius: 30px
+		}
+    </style>
+</head>
+<body>
+<div class="the-box">
+	<img src="./logo.png" width="200">
+    <div class="the-title">Hier entsteht FitCheck</div>
+</div>
+</body>
+</html>
