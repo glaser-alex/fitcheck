8b051dde7a223b038254aececf81f07548a1ae08
Alex Glaser <119404263+glaser-alex@users.noreply.github.com>
2023-10-11 21:29:05 +0200

    styling

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
         }
 
         .the-box {
+            width: 100%;
 			text-align: center;
             position: absolute;
             left: 50%;
@@ -26,10 +27,21 @@
             -ms-transform: translate(-50%, -60%);
         }
 
+        .the-logo {
+            width: 200px;
+            border-radius: 30px;
+        }
+
         .the-title {
             padding: 5px;
             font-size: 2em;
             text-align: center; 
+        }
+        @media (max-width: 700px) {
+            .the-logo {
+                width: 100px;
+                border-radius: 10px;
+            }
         }
     </style>
 </head>
@@ -38,7 +50,7 @@
 <?php include_once "./inc/nav.php" ?>
 
 <div class="the-box">
-	<img style="border-radius: 30px;" src="./img/logo.png" width="200">
+	<img class="the-logo" src="./img/logo.png">
     <div class="the-title">Hier entsteht FitCheck</div>
 </div>
 </body>
