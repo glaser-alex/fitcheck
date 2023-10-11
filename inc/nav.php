d8c69626a44e239b8a59a719e9b2fd55aab824c3
Alex Glaser <119404263+glaser-alex@users.noreply.github.com>
2023-10-11 20:34:26 +0200

    styling

--- inc/nav.php
+++ inc/nav.php
@@ -12,13 +12,15 @@
       <a href="https://<?= $_SERVER['SERVER_NAME']; ?>?jahreszeit=winter">Winter</a>
       <?php
 
-        if ($_COOKIE['username'] == 'admin' || $_SESSION['username'] == 'admin') {
+        if (@$_COOKIE['username'] == 'admin') {
           echo "<a style='color: cyan;' href='https://".$_SERVER['SERVER_NAME']."/administration?action=einträge'>Login Einträge</a>";
           echo "<a href='https://".$_SERVER['SERVER_NAME']."/administration?action=nachrichten'>Nachrichten</a>";
           echo "<a style='color: red;' href='https://".$_SERVER['SERVER_NAME']."/administration?action=erstellen'>Login Tabelle zurücksetzen</a>";
           echo "<a style='color: lime;' href='https://".$_SERVER['SERVER_NAME']."/administration?action=anzeigen'>Login Tabelle anzeigen</a>";
         }
-
+      ?>
+      <span class="space-line"></span>
+      <?php
         if (!isset($_COOKIE['username'])) {
           echo "<a href='https://".$_SERVER['SERVER_NAME']."/login'>Login</a>";
         } else {
